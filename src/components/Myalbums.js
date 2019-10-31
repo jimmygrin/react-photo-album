552c327a5f99a21068bbab639855a5a6651292e1
Jimmy Lin <jimmydlin@gmail.com>
2019-10-31 16:46:17 -0700

    v1

---
+++ src/components/Myalbums.js
@@ -0,0 +1,47 @@
+import React from "react"
+
+function App(props) {
+  return( 
+  <div>
+  <h1>My Albums</h1>
+  <div class="columns">
+        <div class="firstcol">
+            <div class="album">
+            
+            </div>
+            <div class="album">
+         
+            </div>
+      
+        </div>
+  
+    
+        <div class="secondcol">
+             <div class="album">
+
+
+            </div>
+
+            <div class="album">
+
+                
+            </div>
+
+        </div>
+
+        <div class="thirdcol">
+             <div class="album">
+
+            </div>
+
+            <div class="album">
+
+            </div>
+
+        </div>
+
+    </div>
+    </div>
+  )}
+
+export default myalbums
